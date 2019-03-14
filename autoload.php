11275aa0bcbfd07be0a8893959220e580cae9e04
liupan <498501258@qq.com>
2019-03-14 17:37:27 +0800

    初始化

---
+++ autoload.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liupan
+ * Email: 498501258@qq.com
+ * Date: 2019/3/14
+ * Time: 8:58
+ */
+\think\facade\Config::set('template.type', '\mypackages\think\theme\Driver');
